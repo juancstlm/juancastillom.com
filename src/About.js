02eda2eb6b7f3667c1ac0604e1b4d9b34e99628f
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-05-22 23:08:00 -0700

    Style about section

--- src/About.js
+++ src/About.js
@@ -3,7 +3,15 @@ import React from 'react'
 export default class About extends React.Component {
   render() {
     return <section id='about'>
-        <h2>About Me</h2>
+      <div className='container aboutMe'>
+        <div>
+          <h2 className='title heading'>About Me</h2>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat. Donec eu vehicula neque. Donec viverra lorem nunc, tempus euismod eros sollicitudin ut. Quisque et tincidunt libero. Donec id pharetra justo. Proin euismod lacinia dolor, eu scelerisque justo tempus pharetra. Vivamus nunc justo, finibus ut nisl sed, euismod rhoncus nulla. Proin sed magna egestas, egestas ante et, congue eros. In consequat, mauris dapibus tincidunt suscipit, ex libero aliquet diam, at maximus risus enim non leo.
+        </p>
+      </div>
     </section>
   }
 }
