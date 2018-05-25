ebfd9e1c80ac0532c5dcc9b12837198b41a23d2e
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-05-24 20:47:53 -0700

    Move education data to JSON

--- src/Education.js
+++ src/Education.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
+const education = require('./data/education.json')
+
 export default class Education extends React.Component {
+
+  renderEducation(){
+    return education.map((school)=><div className='education-block'>
+      <div className='school-logo'><img src={school.logo}/></div>
+      <div className='school-info'>
+        <h3>{school.name}</h3>
+        <span>{school.startDate} - {school.endDate}</span>
+        <h4>{school.degree}</h4>
+        <p>{school.description}</p>
+      </div>
+
+    </div>)
+  }
   render() {
     return <section id='education' className='one-page-section'>
       <div className='container aboutMe'>
         <div>
           <h2 className='title heading'>Education</h2>
         </div>
-        <div className='education-block'>
-          <h3>San Jose State Univeristy</h3>
-          <span>Aug 2014 - Present</span>
-          <h4>Bachelors of Science in Software Engineering</h4>
-        </div>
+        {this.renderEducation()}
       </div>
     </section>
   }
