0b973f1b50add64a198455aa0f8b94e121d1b65f
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-07-10 20:18:44 -0700

    Remove sections from mobile nav

--- src/About.js
+++ src/About.js
@@ -9,17 +9,11 @@ export default class About extends React.Component {
         </div>
 
         <p>
-          Currently I'm living in San Jose, CA, where I'm pursuing my bachelors degree
+          I'm currently living in San Jose, CA, where I'm pursuing my bachelors degree
           in Software Engineering at San Jose State Univeristy. Appart from my studies
           I enjoy mountain biking. If you want to learn more about the projects  I
           am currently working on, check out them out below.
         </p>
-
-        <div className='map'>
-          <iframe src="https://snazzymaps.com/embed/73391"  title='map' width="100%" height="100%"
-          style={{border: 'none'}}>
-          </iframe>
-        </div>
 
 
       </div>
