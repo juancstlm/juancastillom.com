d082b080320fa90712ca990cebd69cd8727ecd7b
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-07-11 14:59:08 -0700

    Add social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,14 @@ export default class Footer extends React.Component{
   render(){
     return <footer className="jarallax" data-jarallax data-speed="0.2" style={style} >
       <div className='container'>
-
+        <div className='social_links'>
+          <a href='https://www.flickr.com/photos/164979468@N08/'>
+            <img src='./images/social/flickr_logo.svg'></img>
+          </a>
+          <a href='https://github.com/juancstlm'>
+            <img src='./images/social/github_logo.svg'></img>
+          </a>
+        </div>
       </div>
       <div className='copyright clearfix'>
        Copyright © {new Date().getFullYear()} Juan Castillo
