d318bfc18ca633f2bae97b92048bff95467d4fd2
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-12-06 23:26:55 -0800

    remove jarallax

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import {jarallax} from 'jarallax';
-import WordScroller from './WordScroller'
-const style = {backgroundImage: 'url(http://hisham.bitballoon.com/img/bg.jpg)'}
-const languages = ['Hello', 'Hola', 'Здравствуйте!','Hallo' ]
-export default class Header extends React.Component{
-
-  componentDidMount() {
-      jarallax(document.querySelectorAll('.jarallax'));
-    }
-    componentWillUnmount() {
-   jarallax(document.querySelectorAll('.jarallax'), 'destroy');
+import React from "react";
+import WordScroller from "./WordScroller";
+const style = {
+  backgroundImage: "url(http://hisham.bitballoon.com/img/bg.jpg)"
+};
+const languages = ["Hello", "Hola", "Здравствуйте!", "Hallo"];
+export default class Header extends React.Component {
+  render() {
+    return (
+      <section
+        id="home"
+        className="header rellax"
+        style={style}
+        data-jarallax
+        data-speed="0.2"
+      >
+        <div className="container">
+          <div className="middle">
+            <div className="caption">
+              <WordScroller words={languages} />
+              <h1>I Am Juan Castillo</h1>
+              <h6>Software Engineer</h6>
+            </div>
+          </div>
+          <a className="scroll home-s-btn hor-center">
+            <span className="dot center" />
+          </a>
+        </div>
+      </section>
+    );
   }
-    render(){
-        return(
-            <section id='home' className='header jarallax'
-                     style={style} data-jarallax data-speed="0.2">
-                <div className='container'>
-                    <div className='middle'>
-                        <div className='caption'>
-                            <WordScroller words={languages}/>
-                            <h1>I Am Juan Castillo</h1>
-                            <h6>Software Engineer</h6>
-                        </div>
-                    </div>
-                    <a className='scroll home-s-btn hor-center'>
-                      <span className='dot center'></span>
-                    </a>
-                </div>
-            </section>)
-    }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,86 +1,90 @@
-import React from 'react'
+import React from "react";
 
-
-var navbar
-var sticky
-var sections = []
+var navbar;
+var sticky;
+var sections = [];
 export default class Navbar extends React.Component {
-
-  constructor(){
-    super()
-    window.onscroll = () => {this.onScroll()}
+  constructor() {
+    super();
+    window.onscroll = () => {
+      this.onScroll();
+    };
   }
 
-  onScroll(){
+  onScroll() {
     console.log(window.scrollY);
 
     if (window.pageYOffset > sticky) {
-    navbar.classList.add("navbar-sticky")
-  } else {
-    navbar.classList.remove("navbar-sticky");
-  }
-
-  sections = document.getElementsByTagName('section')
-  console.log('sections',sections)
-  for(var i =0; i < sections.length; i++){
-    var id = sections[i].id;
-    var top = sections[i].offsetTop - navbar.offsetTop;
-    var bottom = top + sections[i].offsetHeight;
-
-    if(window.scrollY >= top && window.scrollY <= bottom){
-      navbar.querySelectorAll('a').forEach((node)=>{node.classList.remove('active')})
-
-      var selected = navbar.querySelectorAll('a[href*="#'+id+'"]').forEach((node) => {
-        node.classList.add('active')
-        console.log('node',node);
-      })
-      console.log('selected',selected);
+      navbar.classList.add("navbar-sticky");
+    } else {
+      navbar.classList.remove("navbar-sticky");
     }
-    console.log(sections[i].offsetTop)
-  }
-  }
-componentDidMount(){
-  navbar = document.getElementById("navbar");
 
-  sticky = navbar.offsetTop;
-}
+    sections = document.getElementsByTagName("section");
+    console.log("sections", sections);
+    for (var i = 0; i < sections.length; i++) {
+      var id = sections[i].id;
+      var top = sections[i].offsetTop - navbar.offsetTop;
+      var bottom = top + sections[i].offsetHeight;
+
+      if (window.scrollY >= top && window.scrollY <= bottom) {
+        navbar.querySelectorAll("a").forEach(node => {
+          node.classList.remove("active");
+        });
+
+        var selected = navbar
+          .querySelectorAll('a[href*="#' + id + '"]')
+          .forEach(node => {
+            node.classList.add("active");
+            console.log("node", node);
+          });
+        console.log("selected", selected);
+      }
+      console.log(sections[i].offsetTop);
+    }
+  }
+  componentDidMount() {
+    navbar = document.getElementById("navbar");
+
+    // sticky = navbar.offsetTop;
+  }
 
   render() {
-    return <nav id='navbar' className='navbar'>
-      <div className='container'>
-        <div className='navbar-header'>
-          <a href='#'>
-            <div className='logo'>
-              Juan
-            </div>
-          </a>
+    return (
+      <nav id="navbar" className="navbar">
+        <div className="container">
+          <div className="navbar-header">
+            <a href="#">
+              <div className="logo">Juan</div>
+            </a>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="nav">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experience">Experience</a>
+              </li>
+              <li>
+                <a href="#education">Education</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className='collapse navbar-collapse'>
-          <ul className="nav">
-            <li>
-              <a href='#home'>Home</a>
-            </li>
-            <li>
-              <a href='#about'>About</a>
-            </li>
-            <li>
-              <a href='#experience'>Experience</a>
-            </li>
-            <li>
-              <a href='#education'>Education</a>
-            </li>
-            <li>
-              <a href='#projects'>Projects</a>
-            </li>
-            <li>
-              <a href='#skills'>Skills</a>
-            </li>
-            <li>
-              <a href='#contact'>Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    );
   }
 }
