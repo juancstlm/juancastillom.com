1c4822150511f791d306a272593771290309440e
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-12-19 21:53:53 -0800

    Implement markdown viewer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import marked from 'marked'
-import MarkdownViewer from './MarkdownViewer'
-import content from '../data/footer.md'
+import React from "react";
+import MarkdownViewer from "./MarkdownViewer";
+const contact = require("../data/footer.md");
 
-export default class Footer extends React.Component{
-  render(){
-    console.log(content)
-    const contact = 'You can see more of my work on [GitHub](https://github.com/juancstlm). You can also contact me directly [here]() or email me at [juancstlm@gmail.com](mailto:juancstlm@gmail.com).'
-    return <footer >
-      <div className='container'>
-        <div className='logo'>
-            <img src='./images/logo.png' alt='logo'></img>
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          <div className="logo">
+            <img src="./images/logo.png" alt="logo" />
+          </div>
+          <div>
+            <MarkdownViewer src={contact} />
+          </div>
+          <div className="social_links">
+            <a href="https://www.flickr.com/photos/164979468@N08/">
+              <img src="./images/social/flickr_logo.svg" />
+            </a>
+          </div>
         </div>
-        <div>
-          <MarkdownViewer markdown={contact}/>
-        </div>
-        <div className='social_links'>
-          <a href='https://www.flickr.com/photos/164979468@N08/'>
-            <img src='./images/social/flickr_logo.svg'></img>
-          </a>
-          <a href='https://github.com/juancstlm'>
-            <img src='./images/social/github_logo.svg'></img>
-          </a>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    );
   }
 }
--- src/components/MarkdownViewer.js
+++ src/components/MarkdownViewer.js
@@ -2,8 +2,26 @@ import React from 'react'
 import marked from 'marked'
 
 export default class MarkdownViewer extends React.Component{
+  state = {
+    markdown: '',
+  }
+
+  componentDidMount() {
+    const readmePath = this.props.src
+
+    fetch(readmePath)
+      .then(response => {
+        return response.text()
+      })
+      .then(text => {
+        this.setState({
+          markdown: marked(text)
+        })
+      })
+  }
+
   render() {
-    var markdown = marked(this.props.markdown);
+    const { markdown } = this.state;
     return <div dangerouslySetInnerHTML={{__html:markdown}} />;
   }
 }
