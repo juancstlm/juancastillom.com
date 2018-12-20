1de2e4c8bbe99275c8a1838eba42d45bdac0579c
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-12-19 22:08:33 -0800

    Remove flickr

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,12 +11,7 @@ export default class Footer extends React.Component {
             <img src="./images/logo.png" alt="logo" />
           </div>
           <div>
-            <MarkdownViewer src={contact} />
-          </div>
-          <div className="social_links">
-            <a href="https://www.flickr.com/photos/164979468@N08/">
-              <img src="./images/social/flickr_logo.svg" />
-            </a>
+            <MarkdownViewer src={contact}/>
           </div>
         </div>
       </footer>
