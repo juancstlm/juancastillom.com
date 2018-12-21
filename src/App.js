553658569a1417fe2bc2ce95fd6b76cc95d465ff
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-12-21 12:01:27 -0800

    Remove unused components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import './App.scss'
 import Header from "./components/Header";
 import About from './About'
-import Education from './Education'
-import Projects from './Projects'
-import Contact from './Contact'
-import BehindTheScenes from './components/BehindTheScenes'
 import Footer from './components/Footer'
 
 class App extends Component {
