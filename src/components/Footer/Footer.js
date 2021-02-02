be90e30c76782ba60e18ca07d40ab4ead4a4cb09
Juan Castillo <juancstl@protonmail.com>
2021-02-01 23:52:50 -0800

    Update Greeting and Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
-import MarkdownViewer from "../MarkdownViewer";
-var contact = require("../../data/footer.md");
+// import MarkdownViewer from "../MarkdownViewer";
+// var contact = require("../../data/footer.md");
 
 const Footer = () => {
   return (
@@ -9,7 +9,9 @@ const Footer = () => {
         <div className="logo">
           <img src="./images/logo.png" alt="logo" />
         </div>
-        <MarkdownViewer markdown={contact.default} />
+        {/*<MarkdownViewer markdown={contact.default} />*/}
+        <div><h3>You can see more of my work on <a href="https://github.com/juancstlm">GitHub</a>. You can also contact me directly <a href="mailto:juancstlm@gmail.com">here</a>.</h3>
+        </div>
       </div>
     </footer>
   );
